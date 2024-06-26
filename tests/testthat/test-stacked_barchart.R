e88a1a17e896c5625fe8f4a785896af91fe0c1b6
chen-chen-stat <chen.chen.stat@outlook.com>
2024-06-26 13:32:21 +0000

    Used test_path to locate saved png files in the testing directory

--- tests/testthat/test-stacked_barchart.R
+++ tests/testthat/test-stacked_barchart.R
@@ -11,7 +11,7 @@ plot1 <- stacked_barchart(
 ggsave(plot1, filename = "test_stacked_barchart1.png", width = 7, height = 5)
 
 test_stacked_barchart1 <- readPNG("test_stacked_barchart1.png")
-stacked_barchart1 <- readPNG(test_path("stacked_barchart1.png"))
+stacked_barchart1 <- readPNG(paste0(test_path(),"/snapshots/stacked_barchart1.png"))
 
 test1 <- paste(100 * sum(test_stacked_barchart1 == stacked_barchart1) /
                  length(test_stacked_barchart1), "%")
@@ -33,7 +33,7 @@ plot2 <- stacked_barchart(
 ggsave(plot2, filename = "test_stacked_barchart2.png", width = 7, height = 5)
 
 test_stacked_barchart2 <- readPNG("test_stacked_barchart2.png")
-stacked_barchart2 <- readPNG(test_path("stacked_barchart2.png"))
+stacked_barchart2 <- readPNG(paste0(test_path(),"/snapshots/stacked_barchart2.png"))
 
 test2 <- paste(100 * sum(test_stacked_barchart2 == stacked_barchart2) /
                  length(test_stacked_barchart2), "%")
@@ -52,7 +52,7 @@ plot3 <- stacked_barchart(
 ggsave(plot3, filename = "test_stacked_barchart3.png", width = 7, height = 5)
 
 test_stacked_barchart3 <- readPNG("test_stacked_barchart3.png")
-stacked_barchart3 <- readPNG(test_path("stacked_barchart3.png"))
+stacked_barchart3 <- readPNG(paste0(test_path(),"/snapshots/stacked_barchart3.png"))
 
 test3 <- paste(100 * sum(test_stacked_barchart3 == stacked_barchart3) /
                  length(test_stacked_barchart3), "%")

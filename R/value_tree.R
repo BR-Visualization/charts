e102d08a5f78c5842edf55ef3bd93d37050299ab
Lovemore Gakava <lovemore.gakava@gmail.com>
2024-06-28 14:53:21 +0000

    fix: change default values for value_tree and save_memaid.

--- R/value_tree.R
+++ R/value_tree.R
@@ -7,45 +7,39 @@
 #' @export
 #'
 #' @examples
-#' value_tree()
-value_tree <- function(
-    diagram =
-      "graph LR;
-  A(<B>Benefit-Risk Balance</B>)-->B(<B>Benefits</B>)
-  B-->C(<B>Primary Efficacy</B>)
-  B-->D(<B>Secondary Efficacy</B>)
-  B-->E(<B>Quality of life</B>)
-  C-->F(<B>% Success</B>)
-  D-->G(<B>Mean change</B>)
-  E-->H(<B>Mean change</B>)
-  A-->I(<B>Risks</B>)
-  I-->J(<B>Recurring AE</B>)
-  I-->K(<B>Rare SAE</B>)
-  I-->L(<B>Liver Toxicity</B>)
-  J-->M(<B>Event rate</B>)
-  K-->N(<B>% Event</B>)
-  L-->O(<B>% Event</B>)
-  style A fill:#7ABD7E
-  style B fill:#7ABD7E
-  style I fill:#7ABD7E
-
-  style C fill:#FFE733
-  style D fill:#FFE733
-  style E fill:#FFE733
-  style J fill:#FFE733
-  style K fill:#FFE733
-  style L fill:#C6C6C6
-
-  style F fill: #FFAA1C
-  style G fill: #FFAA1C
-  style H fill: #FFAA1C
-  style M fill: #FFAA1C
-  style N fill: #FFAA1C
-  style O fill: #C6C6C6
-",
-    ...) {
-  DiagrammeR::mermaid(
-    diagram = diagram,
-    ...
-  )
+#' value_tree(
+#' diagram =
+#' "graph LR;
+#' A(<B>Benefit-Risk Balance</B>)-->B(<B>Benefits</B>)
+#' B-->C(<B>Primary Efficacy</B>)
+#' B-->D(<B>Secondary Efficacy</B>)
+#' B-->E(<B>Quality of life</B>)
+#' C-->F(<B>% Success</B>)
+#' D-->G(<B>Mean change</B>)
+#' E-->H(<B>Mean change</B>)
+#' A-->I(<B>Risks</B>)
+#' I-->J(<B>Recurring AE</B>)
+#' I-->K(<B>Rare SAE</B>)
+#' I-->L(<B>Liver Toxicity</B>)
+#' J-->M(<B>Event rate</B>)
+#' K-->N(<B>% Event</B>)
+#' L-->O(<B>% Event</B>)
+#' style A fill:#7ABD7E
+#' style B fill:#7ABD7E
+#' style I fill:#7ABD7E
+#' style C fill:#FFE733
+#' style D fill:#FFE733
+#' style E fill:#FFE733
+#' style J fill:#FFE733
+#' style K fill:#FFE733
+#' style L fill:#C6C6C6
+#' style F fill: #FFAA1C
+#' style G fill: #FFAA1C
+#' style H fill: #FFAA1C
+#' style M fill: #FFAA1C
+#' style N fill: #FFAA1C
+#' style O fill: #C6C6C6
+#' ")
+value_tree <- function(diagram, ...) {
+  DiagrammeR::mermaid(diagram = diagram, ...)
 }

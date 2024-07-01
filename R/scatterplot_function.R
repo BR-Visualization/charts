a3a48df6bc07d4effde61c003632b63c5854c695
abigail <E638936@dir.ucb-group.com>
2024-07-01 09:28:30 -0400

    fix: styler

--- R/scatterplot_function.R
+++ R/scatterplot_function.R
@@ -125,8 +125,7 @@ scatter_plot <- function(df_diff, outcome, legend_position = c(0, 1.05),
     geom_abline(intercept = 0, slope = 1, linetype = 2, size = 1) +
     labs(y = paste("Incremental", outcome[2], " ")) +
     labs(x = paste("Incremental", outcome[1], " ")) +
-    br_charts_theme(
-    ) +
+    br_charts_theme() +
     theme(
       axis.line.x = element_blank(),
       axis.line.y = element_blank(),
